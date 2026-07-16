8d0d554df765b207e8ab58243bc5e22521366dcd
SenixArt <105945599+SenixArt@users.noreply.github.com>
2026-07-16 19:13:02 -0400

    Update Program.cs

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+    private int _top;
+    private int _bottom;
+
+    public Fraction() 
+    { 
+        _top = 1; 
+        _bottom = 1; 
+    }
+
+    public Fraction(int wholeNumber) 
+    { 
+        _top = wholeNumber; 
+        _bottom = 1; 
+    }
+
+    public Fraction(int top, int bottom) 
+    { 
+        _top = top; 
+        _bottom = bottom; 
+    }
+
+    public string GetFractionString() 
+    { 
+        return $"{_top}/{_bottom}"; 
+    }
+
+    public double GetDecimalValue() 
+    { 
+        return (double)_top / (double)_bottom; 
     }
 }
