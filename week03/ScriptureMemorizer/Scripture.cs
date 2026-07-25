fa32f0d5fe56fec7f4b550146a939c6301949d5b
SenixArt <105945599+SenixArt@users.noreply.github.com>
2026-07-24 21:50:00 -0400

    Update Scripture.cs

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 public class Scripture
 {
     private Reference _reference;
-    private List<Word> _words = new List<Word>();
+    private List<Word> _words;
 
     public Scripture(Reference reference, string text)
     {
         _reference = reference;
+        _words = new List<Word>();
+        
+        // Divide el texto en palabras y crea los objetos Word
         string[] splitText = text.Split(' ');
-        foreach (string word in splitText) 
-        { 
-            _words.Add(new Word(word)); 
+        foreach (string wordText in splitText)
+        {
+            _words.Add(new Word(wordText));
         }
     }
 
@@ -20,33 +23,52 @@ public class Scripture
     {
         Random random = new Random();
         int hiddenCount = 0;
-        
-        while (hiddenCount < numberToHide && !IsCompletelyHidden())
+
+        // Filtra solo las palabras que NO están ocultas
+        List<Word> visibleWords = new List<Word>();
+        foreach (Word w in _words)
         {
-            int index = random.Next(_words.Count);
-            if (!_words[index].IsHidden())
+            if (!w.IsHidden())
             {
-                _words[index].Hide();
-                hiddenCount++;
+                visibleWords.Add(w);
             }
+        }
+
+        // Si hay menos palabras visibles que las que queremos ocultar, ajustamos
+        if (visibleWords.Count < numberToHide)
+        {
+            numberToHide = visibleWords.Count;
+        }
+
+        // Oculta palabras aleatorias de las que aún están visibles
+        while (hiddenCount < numberToHide)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index); // La quitamos para no seleccionarla dos veces en el mismo turno
+            hiddenCount++;
         }
     }
 
     public string GetDisplayText()
     {
         string scriptureText = "";
-        foreach (Word word in _words) 
-        { 
-            scriptureText += word.GetDisplayText() + " "; 
+        foreach (Word word in _words)
+        {
+            scriptureText += word.GetDisplayText() + " ";
         }
+        
         return $"{_reference.GetDisplayText()} {scriptureText.Trim()}";
     }
 
     public bool IsCompletelyHidden()
     {
-        foreach (Word word in _words) 
-        { 
-            if (!word.IsHidden()) return false; 
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
         }
         return true;
     }
