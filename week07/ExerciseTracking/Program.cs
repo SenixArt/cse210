ccd35f0b2533ccc66e4ad135e4282235338502e8
SenixArt <105945599+SenixArt@users.noreply.github.com>
2026-08-06 18:17:36 -0400

    Update Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,29 @@
 using System;
 
-class Program
+public abstract class Activity
 {
-    static void Main(string[] args)
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    // Métodos abstractos que cada deporte calculará a su manera
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    // Método general que arma el texto final usando polimorfismo
+    public string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min): Distance {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km";
     }
 }
